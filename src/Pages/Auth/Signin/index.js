9b59d2245609d66ad648a1147bda130af5702d60
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-03-26 13:27:49 +0530

    step 3

--- src/Pages/Auth/Signin/index.js
+++ src/Pages/Auth/Signin/index.js
@@ -2,6 +2,62 @@ import { useState, useEffect, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../Context/AuthContext'
 import { LoginIcon } from '@heroicons/react/outline'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 450px;
+  height: 400px;
+  margin: auto;
+  border: 2px solid #fccf03;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const Button = styled.button`
+  background-color: #fccf03;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+  width: 100%;
+  height: 30px;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const H1 = styled.h1`
+font-size: 50px;
+margin-top: 30px;
+padding: 0px 60px;
+justify-content: center;
+`
+
+const StyledForm = styled.form`
+`
+
+const User = styled.input`
+   margin-top: 20px;
+   width: 100%;
+   height: 35px;
+   border-radius: 10px;
+   padding:10px;
+   color:black;
+`
+const Div = styled.div`
+  margin-top: 15px;
+  span{
+    display: flex;
+    flex-direction: row;
+  }
+  p{
+    color: #fccf03;
+    padding-left: 5px;
+  }
+`
 
 const Signin = () => {
 
@@ -25,25 +81,26 @@ const Signin = () => {
   }
 
   const navigate = useNavigate()
-  
+
   useEffect(() => {
-    loggedIn && navigate('/')
+    loggedIn && navigate('/hashmart')
   }, [loggedIn])
 
+
+
   return (
-    <div>
+    <Container>
       <div>
         <div>
-          <h2>Login</h2>
+          <H1>Login</H1>
         </div>
-        <form
+        <StyledForm
           autoComplete="off"
           onSubmit={handleSignIn}
         >
           <div>
             <div>
-              <label>Email</label>
-              <input
+              <User
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
@@ -53,8 +110,7 @@ const Signin = () => {
               />
             </div>
             <div>
-              <label>Password</label>
-              <input
+              <User
                 type="Password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
@@ -64,26 +120,26 @@ const Signin = () => {
               />
             </div>
             <div>
-              <div>
+              <Div>
                 <span>
                   Don't have an account? Sign up{" "}
                   <Link to="/signup">
-                    {" "}
-                    here.
+                    
+                    <p>{" "} here</p>
                   </Link>
                 </span>
-              </div>
+              </Div>
             </div>
             <div>
-              <button type="submit">
-                <LoginIcon aria1-hidden="true" />
+              <Button type="submit">
+                <LoginIcon aria1-hidden="true"  className="h-6 w-6" />
                 Login
-              </button>
+              </Button>
             </div>
           </div>
-        </form>
+        </StyledForm>
       </div>
-    </div>
+    </Container>
   )
 }
 
--- src/Pages/Auth/Signup/index.js
+++ src/Pages/Auth/Signup/index.js
@@ -3,6 +3,64 @@ import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../Context/AuthContext'
 import validations from './validations'
+import styled from 'styled-components'
+
+const Container = styled.div` 
+  max-width: 450px;
+  height: 500px;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+  border: 2px solid #fccf03;
+  border-radius: 20px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+`
+const H1 = styled.h1`
+  font-size: 50px;
+  margin-top: 20px;
+  padding: 0px 60px;
+  justify-content: center;
+`
+
+const StyledForm = styled.form`
+`
+
+const Input = styled.input`
+   margin-top: 20px;
+   width: 100%;
+   height: 35px;
+   border-radius: 5px;
+   padding:10px;
+   color:black;
+`
+
+const Div = styled.div`
+  margin-top: 15px;
+  span{
+    display: flex;
+    flex-direction: row;
+  }
+  p{
+    color: #fccf03;
+    padding-left: 5px;
+  }
+`
+
+const Button = styled.button`
+  background-color: #fccf03;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+  width: 100%;
+  height: 20px;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
 
 const Signup = () => {
   const {
@@ -16,9 +74,9 @@ const Signup = () => {
   } = useAuth()
 
   const navigate = useNavigate()
-  
+
   useEffect(() => {
-    loggedIn && navigate('/')
+    loggedIn && navigate('/hashmart')
   }, [loggedIn])
 
   const handleSignUpFormChange = (e) => {
@@ -27,107 +85,103 @@ const Signup = () => {
 
   const handleSignUpSubmit = (e) => {
     e.preventDefault()
-    setErrors(validations(currentUser, users)) 
+    setErrors(validations(currentUser, users))
     setIsSubmitting(true)
     localStorage.setItem('user', JSON.stringify(currentUser))
     localStorage.setItem('users', JSON.stringify(users))
   }
 
   return (
-    <div >
+    <Container >
       <div >
         <div>
-          <h2 >Sign Up</h2>
+          <H1 >Sign Up</H1>
         </div>
-        <form
+        <StyledForm
           autoComplete="off"
           onSubmit={handleSignUpSubmit}
         >
           <div >
             <div>
-            {errors.firstName && <span>{errors.firstName}</span>}
-              <label >First Name</label>
-              <input
+              {errors.firstName && <span>{errors.firstName}</span>}
+              <Input
                 type="text"
                 onChange={handleSignUpFormChange}
                 value={currentUser.firstName}
                 name="firstName"
                 placeholder="First Name"
               />
-              
+
             </div>
 
             <div>
-            {errors.lastName && <span>{errors.lastName}</span>}
-              <label>Last Name</label>
-              <input
+              {errors.lastName && <span>{errors.lastName}</span>}
+              <Input
                 type="text"
                 onChange={handleSignUpFormChange}
                 value={currentUser.lastName}
                 name="lastName"
                 placeholder="Last Name"
               />
-              
+
             </div>
             <div>
-            {errors.email && <span >{errors.email}</span>}
-              <label>Email</label>
-              <input
+              {errors.email && <span >{errors.email}</span>}
+              <Input
                 type="email"
                 onChange={handleSignUpFormChange}
                 value={currentUser.email}
                 name="email"
                 placeholder="Email Address"
               />
-              
+
             </div>
             <div>
-            {errors.password && <span >{errors.password}</span>}
-              <label >Password</label>
-              <input
+              {errors.password && <span >{errors.password}</span>}
+              <Input
                 type="Password"
                 onChange={handleSignUpFormChange}
                 value={currentUser.password}
                 name="password"
                 placeholder="Password"
               />
-              
+
             </div>
             <div>
-            {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
-              <label >Password Confirm</label>
-              <input
+              {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
+              <Input
                 type="Password"
                 onChange={handleSignUpFormChange}
                 value={currentUser.passwordConfirm}
                 name="passwordConfirm"
-                placeholder="Password Confirm"
+                placeholder="Confirm Password"
               />
-              
+
             </div>
             <div >
-              <div >
+              <Div >
                 <span>
                   Already have an account? Login{" "}
                   <Link to="/signin" >
-                    {" "}
+                    <p>{" "}
                     here.
+                    </p>
                   </Link>
                 </span>
-              </div>
+              </Div>
             </div>
             <div >
-              <button type="submit" >
-                <IdentificationIcon
+              <Button type="submit" >
+                <IdentificationIcon className="h-6 w-6"
                   aria1-hidden="true"
                 />
                 Sign Up
-              </button>
+              </Button>
             </div>
           </div>
-        </form>
+        </StyledForm>
       </div>
-    </div>
+    </Container>
   )
 }
 
